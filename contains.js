5b9399060df8904e04ac2300227bca0b1819bd88
dreamingrainbow <michaeladennis@yahoo.com>
2017-11-11 20:09:40 -0700

    Update contains.js

--- contains.js
+++ contains.js
@@ -14,7 +14,7 @@ const contains = (item, list, cb = x => { return x }) => {
     return cb(false);    
 };
 (() => {
-document.getElementById('result').append('Loaded<br\>');
+document.getElementById('result').append('Loaded</br>');
 document.getElementById('result').append(contains('x', '<ukljh;lojhhje') ? 'Yes x found</br>' : 'No x found</br>'); // false
 document.getElementById('result').append(contains('x', '<ukljh;xlojhhje') ? 'Yes x found</br>' : 'No x found</br>' );
 })();
