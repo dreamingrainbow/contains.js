6b96f5ee52d5f77754b2baaee2cfe9e5654618d9
dreamingrainbow <michaeladennis@yahoo.com>
2017-11-11 18:33:54 -0700

    Create contains.js

---
+++ contains.js
@@ -0,0 +1,15 @@
+contains = (item, list, cb = x => { return x }) => {
+
+    /* Start looping through the list */
+    for (let i = 0; i < list.length; i++) {
+        if(typeof list[i] === 'object' ) return 
+        /* Each Loop check if the item "item" passed in is the same as the current item "list[i]" in the loop */
+        if (item === list[i]) {
+            /* if both the item and the list[i] item match pass cb true */
+            return cb(true);
+        }
+        /* Exit the loop */
+    };
+    /* since we have not return true must not be there */
+    return cb(false);    
+};
